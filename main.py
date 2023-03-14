854300e4322b029279e82cc13eeb23d93eae20bd
Mark Harmon <neothej1sec@gmail.com>
2023-03-14 13:42:51 -0500

    added cpu usage loading bar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import psutil
 from tkinter import *
+import tkinter.ttk as ttk
 from tkinter import messagebox as mb
 
 
@@ -34,6 +36,15 @@ def format_time(time):
     centiseconds = int(time - (minutes * 6000) - (seconds * 100))
     return f"{minutes:02}:{seconds:02}.{centiseconds:02}"
 
+def get_cpu_usage():
+    cpu_percent = psutil.cpu_percent()
+    return cpu_percent
+
+def update_loading_bar(loading_bar):
+    cpu_percent = get_cpu_usage()
+    loading_bar['value'] = cpu_percent
+    loading_bar.update()
+
 stopwatch_label = Label(root, text="00:00.00", font=("Arial", 20), borderwidth=2, relief="solid", padx=5, pady=5)
 stopwatch_label.place(x=120, y=50)
 
@@ -46,5 +57,8 @@ pause_button.place(x=110, y=110)
 reset_button = Button(root, text="Reset", command=reset_stopwatch)
 reset_button.place(x=190, y=110)
 
+loading_bar = ttk.Progressbar(root, orient="horizontal", length=200, mode="determinate")
+loading_bar.pack(pady=10)
+root.after(1000, update_loading_bar, loading_bar)
 if __name__ == '__main__':
     root.mainloop()
